1277ccfb1fda0ea162f900d76cfd789f7ce003e6
klugh <klugh@student.ethz.ch>
2021-04-16 08:52:05 +0200

    added flags utils

--- mmvae_mst/flags.py
+++ mmvae_mst/flags.py
@@ -1,10 +1,11 @@
-
-import argparse
-from mmvae_mst.utils.BaseFlags import parser as parser
+from mmvae_base import log
+from mmvae_base.BaseFlags import parser as parser
+from mmvae_base.utils.filehandling import get_method
+from mmvae_base.utils.flags_utils import get_freer_gpu, update_flags_with_config
 
 # DATASET NAME
 parser.add_argument('--dataset', type=str, default='SVHN_MNIST_text', help="name of the dataset")
-
+parser.add_argument('--exp_str_prefix', type=str, default='MST', help="prefix of the experiment directory.")
 # DATA DEPENDENT
 # to be set by experiments themselves
 parser.add_argument('--style_m1_dim', type=int, default=0, help="dimension of varying factor latent space")
@@ -35,8 +36,48 @@ parser.add_argument('--clf_save_m3', type=str, default='clf_m3', help="model sav
 parser.add_argument('--beta_m1_style', type=float, default=1.0, help="default weight divergence term style modality 1")
 parser.add_argument('--beta_m2_style', type=float, default=1.0, help="default weight divergence term style modality 2")
 parser.add_argument('--beta_m3_style', type=float, default=1.0, help="default weight divergence term style modality 2")
-parser.add_argument('--div_weight_m1_content', type=float, default=0.25, help="default weight divergence term content modality 1")
-parser.add_argument('--div_weight_m2_content', type=float, default=0.25, help="default weight divergence term content modality 2")
-parser.add_argument('--div_weight_m3_content', type=float, default=0.25, help="default weight divergence term content modality 2")
-parser.add_argument('--div_weight_uniform_content', type=float, default=0.25, help="default weight divergence term prior")
+parser.add_argument('--div_weight_m1_content', type=float, default=0.25,
+                    help="default weight divergence term content modality 1")
+parser.add_argument('--div_weight_m2_content', type=float, default=0.25,
+                    help="default weight divergence term content modality 2")
+parser.add_argument('--div_weight_m3_content', type=float, default=0.25,
+                    help="default weight divergence term content modality 2")
+parser.add_argument('--div_weight_uniform_content', type=float, default=0.25,
+                    help="default weight divergence term prior")
 
+
+def setup_flags(flags, testing=False):
+    """
+    If testing is true, no cli arguments will be read.
+    """
+    import torch
+    from pathlib import Path
+    import numpy as np
+    if flags.config_path:
+        flags = update_flags_with_config(config_path=flags.config_path, testing=testing)
+    # flags = expand_paths(flags)
+    use_cuda = torch.cuda.is_available()
+    flags.device = torch.device('cuda' if use_cuda else 'cpu')
+    if str(flags.device) == 'cuda':
+        torch.cuda.set_device(get_freer_gpu())
+    flags = flags_set_alpha_modalities(flags)
+    flags.log_file = log.manager.root.handlers[1].baseFilename
+    flags.len_sequence = 128 if flags.text_encoding == 'word' else 1024
+
+    if flags.load_flags:
+        old_flags = torch.load(Path(flags.load_flags).expanduser())
+        # create param dict from all the params of old_flags that are not paths
+        params = {k: v for k, v in old_flags.item() if ('dir' not in v) and ('path' not in v)}
+        flags.__dict__.update(params)
+
+    if not flags.seed:
+        # set a random seed
+        flags.seed = np.random.randint(0, 10000)
+    flags = get_method(flags)
+    return flags
+
+
+def flags_set_alpha_modalities(flags):
+    flags.alpha_modalities = [flags.div_weight_uniform_content, flags.div_weight_m1_content,
+                              flags.div_weight_m2_content, flags.div_weight_m3_content]
+    return flags
