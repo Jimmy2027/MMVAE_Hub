7b4a01ea7bd11c2a7fea705b2ff4278d0a859758
klugh <klugh@student.ethz.ch>
2021-04-16 08:51:12 +0200

    added test_training.py

---
+++ tests/test_training.py
@@ -0,0 +1,32 @@
+import tempfile
+from pathlib import Path
+
+import torch
+from mmvae_base.modalities.text.alphabet import alphabet
+from mmvae_base.utils.filehandling import create_dir_structure
+
+import mmvae_mst
+from mmvae_mst import MNISTSVHNText, run_epochs
+from mmvae_mst.flags import flags_set_alpha_modalities, setup_flags, parser
+
+
+def test_run_epochs():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        flags = parser.parse_args([])
+        flags.config_path = Path(mmvae_mst.__file__).parent.parent / 'configs/toy_config.json'
+        flags = setup_flags(flags, testing=True)
+        use_cuda = torch.cuda.is_available()
+        flags.device = torch.device('cuda' if use_cuda else 'cpu')
+        flags.dir_experiment = tmpdirname
+        flags = flags_set_alpha_modalities(flags)
+        flags.alphabet = alphabet
+        flags = create_dir_structure(flags)
+
+        mst = MNISTSVHNText(flags, alphabet)
+        mst.set_optimizer()
+        run_epochs(mst)
+
+
+if __name__ == '__main__':
+    pass
+    # test_run_epochs()
