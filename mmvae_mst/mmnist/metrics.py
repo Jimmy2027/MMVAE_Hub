f50bc0e3cfc7408f1ca3fa4489dfe096cbbb4ba5
klugh <klugh@student.ethz.ch>
2021-04-18 18:00:34 +0200

    added mmnist related code

---
+++ mmvae_mst/mmnist/metrics.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import typing
+
+import torch
+from mmvae_base.utils import BaseMetrics
+from sklearn.metrics import accuracy_score
+
+
+class MmnistMetrics(BaseMetrics):
+    """
+    Defines a set of metrics that are used to evaluate the performance of a model
+    Modified version of https://github.com/ParGG/MasterThesisOld/blob/44f7b93214fa16494ebaeef7763ff81943b5ffc3/losses.py#L142
+    """
+
+    def __init__(self, prediction: torch.Tensor, groundtruth: torch.Tensor, str_labels):
+        super(MmnistMetrics, self).__init__(prediction, groundtruth, str_labels)
+
+    def evaluate(self) -> typing.Dict[str, list]:
+        """
+        Computes the different metrics.
+        """
+
+        return {**{
+            'accuracy': [accuracy_score(self.groundtruth, self.prediction)],
+        }, **self.get_counts()}
