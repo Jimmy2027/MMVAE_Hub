70b6ab7f664cbdde69fe9f5efbc28384886962ed
klugh <klugh@student.ethz.ch>
2021-04-18 18:04:46 +0200

    added test_run_epochs

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,32 +1,47 @@
 import tempfile
 from pathlib import Path
 
+import pytest
 import torch
 from mmvae_base.modalities.text.alphabet import alphabet
-from mmvae_base.utils.filehandling import create_dir_structure
+from mmvae_base.utils.plotting import generate_plots
 
 import mmvae_mst
-from mmvae_mst import MNISTSVHNText, run_epochs
-from mmvae_mst.flags import flags_set_alpha_modalities, setup_flags, parser
+from mmvae_mst import run_epochs
+from mmvae_mst.mmnist.experiment import MMNISTExperiment
+# from mmvae_mst.mnistsvhntext.flags import flags_set_alpha_modalities, setup_flags, parser
+from mmvae_mst.mmnist.flags import FlagsSetup, parser
 
 
+@pytest.mark.tox
 def test_run_epochs():
     with tempfile.TemporaryDirectory() as tmpdirname:
         flags = parser.parse_args([])
-        flags.config_path = Path(mmvae_mst.__file__).parent.parent / 'configs/toy_config.json'
-        flags = setup_flags(flags, testing=True)
+        config_path = Path(mmvae_mst.__file__).parent.parent / 'configs/toy_config.json'
+        flags_setup = FlagsSetup(config_path)
+        flags = flags_setup.setup(flags, testing=True)
         use_cuda = torch.cuda.is_available()
         flags.device = torch.device('cuda' if use_cuda else 'cpu')
         flags.dir_experiment = tmpdirname
-        flags = flags_set_alpha_modalities(flags)
-        flags.alphabet = alphabet
-        flags = create_dir_structure(flags)
+        mst = MMNISTExperiment(flags, alphabet)
+        mst.set_optimizer()
+        run_epochs(mst)
 
-        # mst = MNISTSVHNText(flags, alphabet)
-        # mst.set_optimizer()
-        # run_epochs(mst)
+
+def test_generate_plots():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        flags = parser.parse_args([])
+        config_path = Path(mmvae_mst.__file__).parent.parent / 'configs/toy_config.json'
+        flags_setup = FlagsSetup(config_path)
+        flags = flags_setup.setup(flags, testing=True)
+        use_cuda = torch.cuda.is_available()
+        flags.device = torch.device('cuda' if use_cuda else 'cpu')
+        flags.dir_experiment = tmpdirname
+        mst = MMNISTExperiment(flags, alphabet)
+        generate_plots(mst, epoch=1)
 
 
 if __name__ == '__main__':
-    pass
-    # test_run_epochs()
+    # pass
+    test_run_epochs()
+    # test_generate_plots()
