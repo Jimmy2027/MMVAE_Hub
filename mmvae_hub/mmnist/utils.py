a9b4ed107fd11ee2c4157964f9f1a8f66ad1bf71
klugh <klugh@student.ethz.ch>
2021-04-19 15:19:45 +0200

    using zipfile

--- mmvae_hub/mmnist/utils.py
+++ mmvae_hub/mmnist/utils.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-import os
 import tempfile
+import zipfile
 from pathlib import Path
+
 import wget
 
-def download_polymnist_clfs(out_path: Path):
-    url = "https://www.dropbox.com/sh/esbj7b6bg99jxna/AAAIa943_mueszIwh5721UMXa?dl=1"
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        wget.download(out=f'{tmpdirname}/{out_path.stem}.zip', url=url)
 
-        unzip_command = f'unzip {tmpdirname}/{out_path.stem + ".zip"} -d {out_path}/'
-        os.system(unzip_command)
+def download_polymnist_clfs(out_path: Path):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        zip_file = f'{tmpdirname}/{out_path.stem}.zip'
+        url = "https://www.dropbox.com/sh/esbj7b6bg99jxna/AAAIa943_mueszIwh5721UMXa?dl=1"
+        wget.download(out=zip_file, url=url)
+        with zipfile.ZipFile(zip_file) as z:
+            z.extractall(out_path)
 
     assert out_path.exists()
