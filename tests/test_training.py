4d400c1346ef766ee482875dbac1eb91392d21e8
klugh <klugh@student.ethz.ch>
2021-04-19 17:38:58 +0200

    cleanup

--- tests/test_training.py
+++ tests/test_training.py
@@ -2,55 +2,42 @@ import tempfile
 from pathlib import Path
 
 import pytest
-import torch
 
 import mmvae_hub
 from mmvae_hub.base.evaluation.eval_metrics.coherence import test_generation
-from mmvae_hub.base.modalities.text.alphabet import alphabet
 from mmvae_hub.base.utils.plotting import generate_plots
 from mmvae_hub.mmnist import MmnistTrainer
 from mmvae_hub.mmnist.experiment import MMNISTExperiment
-# from mmvae_hub.mnistsvhntext.flags import flags_set_alpha_modalities, setup_flags, parser
 from mmvae_hub.mmnist.flags import FlagsSetup, parser
+
+
+def set_me_up(tmpdirname):
+    flags = parser.parse_args([])
+    config_path = Path(mmvae_hub.__file__).parent.parent / 'configs/toy_config.json'
+    flags_setup = FlagsSetup(config_path)
+    flags = flags_setup.setup_test(flags, tmpdirname)
+    mst = MMNISTExperiment(flags)
+    mst.set_optimizer()
+    return mst
 
 
 @pytest.mark.tox
 def test_run_epochs_mmnist():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        flags = parser.parse_args([])
-        config_path = Path(mmvae_hub.__file__).parent.parent / 'configs/toy_config.json'
-        flags_setup = FlagsSetup(config_path)
-        flags = flags_setup.setup_test(flags, tmpdirname)
-        mst = MMNISTExperiment(flags, alphabet)
-        mst.set_optimizer()
+        mst = set_me_up(tmpdirname)
         trainer = MmnistTrainer(mst)
         trainer.run_epochs()
 
 
 def test_generate_plots():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        flags = parser.parse_args([])
-        config_path = Path(mmvae_hub.__file__).parent.parent / 'configs/toy_config.json'
-        flags_setup = FlagsSetup(config_path)
-        flags = flags_setup.setup(flags, testing=True)
-        use_cuda = torch.cuda.is_available()
-        flags.device = torch.device('cuda' if use_cuda else 'cpu')
-        flags.dir_experiment = tmpdirname
-        flags.dir_fid = tmpdirname + '/fid'
-        mst = MMNISTExperiment(flags, alphabet)
+        mst = set_me_up(tmpdirname)
         generate_plots(mst, epoch=1)
 
 
 def test_test_generation():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        flags = parser.parse_args([])
-        config_path = Path(mmvae_hub.__file__).parent.parent / 'configs/toy_config.json'
-        flags_setup = FlagsSetup(config_path)
-        flags = flags_setup.setup(flags, testing=True)
-        use_cuda = torch.cuda.is_available()
-        flags.device = torch.device('cuda' if use_cuda else 'cpu')
-        flags.dir_experiment = tmpdirname
-        mst = MMNISTExperiment(flags, alphabet)
+        mst = set_me_up(tmpdirname)
         test_generation(mst)
 
 
