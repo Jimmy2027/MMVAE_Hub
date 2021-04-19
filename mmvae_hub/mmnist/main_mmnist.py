55eb2349e91a48dbd676ea1e7cfe599891994159
klugh <klugh@student.ethz.ch>
2021-04-19 17:31:26 +0200

    new main_mmnist.py

--- mmvae_hub/mmnist/main_mmnist.py
+++ mmvae_hub/mmnist/main_mmnist.py
@@ -1,47 +1,13 @@
-import sys
-import os
-import json
-
-import torch
-
-from run_epochs import run_epochs
-
-from utils.filehandling import create_dir_structure
-from mmnist.flags import parser
-from mmnist.experiment import MMNISTExperiment
+from mmvae_hub.base.utils.flags_utils import get_config_path
+from mmvae_hub.mmnist import MmnistTrainer
+from mmvae_hub.mmnist.experiment import MMNISTExperiment
+from mmvae_hub.mmnist.flags import FlagsSetup, parser
 
 if __name__ == '__main__':
-    FLAGS = parser.parse_args()
-    use_cuda = torch.cuda.is_available()
-    FLAGS.device = torch.device('cuda' if use_cuda else 'cpu')
-
-    if FLAGS.method == 'poe':
-        FLAGS.modality_poe = True
-        FLAGS.poe_unimodal_elbos = True
-    elif FLAGS.method == 'moe':
-        FLAGS.modality_moe = True
-    elif FLAGS.method == 'jsd':
-        FLAGS.modality_jsd = True
-    else:
-        print('method implemented...exit!')
-        sys.exit()
-
-    # postprocess flags
-    assert len(FLAGS.unimodal_datapaths_train) == len(FLAGS.unimodal_datapaths_test)
-    FLAGS.num_mods = len(FLAGS.unimodal_datapaths_train)  # set number of modalities dynamically
-    if FLAGS.div_weight_uniform_content is None:
-        FLAGS.div_weight_uniform_content = 1 / (FLAGS.num_mods + 1)
-    FLAGS.alpha_modalities = [FLAGS.div_weight_uniform_content]
-    if FLAGS.div_weight is None:
-        FLAGS.div_weight = 1 / (FLAGS.num_mods + 1)
-    FLAGS.alpha_modalities.extend([FLAGS.div_weight for _ in range(FLAGS.num_mods)])
-    print("alpha_modalities:", FLAGS.alpha_modalities)
-    create_dir_structure(FLAGS)
-
-    alphabet_path = os.path.join(os.getcwd(), 'alphabet.json')
-    with open(alphabet_path) as alphabet_file:
-        alphabet = str(''.join(json.load(alphabet_file)))
-    mst = MMNISTExperiment(FLAGS, alphabet)
+    flags = parser.parse_args()
+    flags_setup = FlagsSetup(get_config_path(flags))
+    flags = flags_setup.setup(flags)
+    mst = MMNISTExperiment(flags)
     mst.set_optimizer()
-
-    run_epochs(mst)
+    trainer = MmnistTrainer(mst)
+    trainer.run_epochs()
