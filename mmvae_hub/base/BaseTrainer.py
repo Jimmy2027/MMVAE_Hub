ba3813ae0880890f689403b51d01b8ced0414b30
klugh <klugh@student.ethz.ch>
2021-04-29 11:49:34 +0200

    remove deprecated BaseTrainer_

--- mmvae_hub/base/BaseTrainer.py
+++ mmvae_hub/base/BaseTrainer.py
@@ -16,7 +16,6 @@ from mmvae_hub.base.evaluation.eval_metrics.representation import train_clf_lr_a
 from mmvae_hub.base.evaluation.eval_metrics.sample_quality import calc_prd_score
 from mmvae_hub.base.experiment_vis.utils import run_notebook_convert
 from mmvae_hub.base.utils import BaseTBLogger
-from mmvae_hub.base.utils.Dataclasses import *
 from mmvae_hub.base.utils.average_meters import *
 from mmvae_hub.base.utils.plotting import generate_plots
 from mmvae_hub.base.utils.utils import save_and_log_flags, at_most_n, get_items_from_dict
@@ -163,102 +162,6 @@ class BaseTrainer:
                     test_results['prd_scores'] = prd_scores
 
         return {k: v for k, v in test_results.items() if k in ['total_loss', 'lr_eval', 'text_gen_eval']}
-
-    def finalize(self):
-        run_notebook_convert(self.flags.dir_experiment_run)
-
-
-class BaseTrainer_:
-    def __init__(self, exp: BaseExperiment):
-        self.exp = exp
-        self.flags = exp.flags
-        self.tb_logger = self._setup_tblogger()
-        self.callback = self._set_callback()
-
-    def _setup_tblogger(self):
-        writer = SummaryWriter(self.flags.dir_logs)
-        tb_logger = BaseTBLogger(self.flags.str_experiment, writer)
-        str_flags = save_and_log_flags(self.flags)
-        tb_logger.writer.add_text('FLAGS', str_flags, 0)
-        return tb_logger
-
-    @abstractmethod
-    def _set_callback(self):
-        pass
-
-    def run_epochs(self):
-        for epoch in tqdm(range(self.flags.start_epoch, self.flags.end_epoch), postfix='epochs'):
-            # set the epoch for tb_logger
-            self.tb_logger.step = epoch
-
-            # one epoch of training and testing
-            self.train()
-            self.test(epoch)
-            self.callback.update_epoch(epoch)
-
-        self.finalize()
-
-    def train(self):
-        self.exp.mm_vae.train()
-
-        d_loader = DataLoader(self.exp.dataset_train, batch_size=self.flags.batch_size, shuffle=True,
-                              num_workers=self.flags.dataloader_workers,
-                              drop_last=True)
-        training_steps = self.flags.steps_per_training_epoch
-
-        for iteration, (batch_d, _) in enumerate(at_most_n(d_loader, training_steps)):
-            basic_routine = self.basic_routine_epoch(batch_d)
-            results = basic_routine['results']
-            total_loss = basic_routine['total_loss']
-            klds = basic_routine['klds']
-            log_probs = basic_routine['log_probs']
-            # backprop
-            self.exp.optimizer.zero_grad()
-            total_loss.backward()
-            self.exp.optimizer.step()
-            self.tb_logger.write_training_logs(results, total_loss, log_probs, klds)
-
-    def test(self, epoch):
-        with torch.no_grad():
-            self.exp.mm_vae.eval()
-
-            d_loader = DataLoader(self.exp.dataset_test, batch_size=self.flags.batch_size, shuffle=True,
-                                  num_workers=self.flags.dataloader_workers, drop_last=True)
-            training_steps = self.flags.steps_per_training_epoch
-
-            for iteration, (batch_d, _) in enumerate(at_most_n(d_loader, training_steps)):
-                basic_routine = self.basic_routine_epoch(batch_d)
-                results = basic_routine['results']
-                total_loss = basic_routine['total_loss']
-                klds = basic_routine['klds']
-                log_probs = basic_routine['log_probs']
-                self.tb_logger.write_testing_logs(results, total_loss, log_probs, klds)
-
-            log.info('generating plots')
-            plots = generate_plots(self.exp, epoch)
-            self.tb_logger.write_plots(plots, epoch)
-
-            if (epoch + 1) % self.flags.eval_freq == 0 or (epoch + 1) == self.flags.end_epoch:
-                if self.flags.eval_lr:
-                    log.info('evaluation of latent representation')
-                    clf_lr = train_clf_lr_all_subsets(self.exp)
-                    lr_eval = test_clf_lr_all_subsets(clf_lr, self.exp)
-                    self.tb_logger.write_lr_eval(lr_eval)
-
-                if self.flags.use_clf:
-                    log.info('test generation')
-                    gen_eval = test_generation(self.exp)
-                    self.tb_logger.write_coherence_logs(gen_eval)
-
-                if self.flags.calc_nll:
-                    log.info('estimating likelihoods')
-                    lhoods = estimate_likelihoods(self.exp)
-                    self.tb_logger.write_lhood_logs(lhoods)
-
-                if self.flags.calc_prd and ((epoch + 1) % self.flags.eval_freq_fid == 0):
-                    log.info('calculating prediction score')
-                    prd_scores = calc_prd_score(self.exp)
-                    self.tb_logger.write_prd_scores(prd_scores)
 
     def finalize(self):
         run_notebook_convert(self.flags.dir_experiment_run)
