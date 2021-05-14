97a6a71be695a5181e8ffcf2a30f889ed067ab49
klugh <klugh@student.ethz.ch>
2021-05-14 13:59:53 +0200

    update

--- mmvae_hub/polymnist/main_polymnist.py
+++ mmvae_hub/polymnist/main_polymnist.py
@@ -6,17 +6,32 @@ from mmvae_hub.polymnist.experiment import PolymnistExperiment
 from mmvae_hub.polymnist.flags import FlagsSetup, parser
 
 search_spaces = {
-    'method': ['moe', 'joint_elbo'],
-    'class_dim': [32, 64, 128, 256, 512],
-    "beta": [0.5, 1, 2.5, 3, 5]
+    'method': ['planar_mixture'],
+    # 'method': ['moe'],
+    'class_dim': [512],
+    "beta": [2.5],
+    "num_flows": [0],
+    "num_mods": [1]
+}
+
+search_spaces_1 = {
+    'method': ['planar_mixture'],
+    # 'method': ['moe'],
+    'class_dim': [512],
+    "beta": [2.5],
+    "num_flows": [5],
+    "num_mods": [3]
 }
 
 if __name__ == '__main__':
-    for sp in ParameterGrid(search_spaces):
-        flags = parser.parse_args()
-        flags_setup = FlagsSetup(get_config_path(flags))
-        flags = flags_setup.setup(flags, additional_args=sp)
-        mst = PolymnistExperiment(flags)
-        mst.set_optimizer()
-        trainer = PolymnistTrainer(mst)
-        trainer.run_epochs()
+    for grid in [search_spaces_1]:
+        for sp in ParameterGrid(grid):
+            # for _ in [1]:
+            flags = parser.parse_args()
+            flags_setup = FlagsSetup(get_config_path(flags))
+            flags = flags_setup.setup(flags, additional_args=sp)
+            # flags = flags_setup.setup(flags)
+            mst = PolymnistExperiment(flags)
+            mst.set_optimizer()
+            trainer = PolymnistTrainer(mst)
+            trainer.run_epochs()
