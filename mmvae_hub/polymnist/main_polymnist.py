82a5db5b5e3337ab3516a3188f89585e03102d16
klugh <klugh@student.ethz.ch>
2021-05-19 15:39:01 +0200

    add norby contextmanager

--- mmvae_hub/polymnist/main_polymnist.py
+++ mmvae_hub/polymnist/main_polymnist.py
@@ -1,3 +1,4 @@
+from norby.utils import norby
 from sklearn.model_selection import ParameterGrid
 
 from mmvae_hub.base.utils.flags_utils import get_config_path
@@ -6,32 +7,36 @@ from mmvae_hub.polymnist.experiment import PolymnistExperiment
 from mmvae_hub.polymnist.flags import FlagsSetup, parser
 
 search_spaces = {
-    'method': ['planar_mixture'],
-    # 'method': ['moe'],
+    'method': ['planar_mixture', 'pfom'],
+    # 'method': ['joint_elbo'],
     'class_dim': [512],
     "beta": [2.5],
-    "num_flows": [0],
-    "num_mods": [1]
+    "num_flows": [5],
+    "num_mods": [3],
+    "end_epoch": [100],
+    "weighted_mixture": [True, False]
 }
 
 search_spaces_1 = {
-    'method': ['planar_mixture'],
+    'method': ['pfom'],
     # 'method': ['moe'],
     'class_dim': [512],
     "beta": [2.5],
     "num_flows": [5],
-    "num_mods": [3]
+    "num_mods": [3],
+    "weighted_mixture": [True, False]
 }
 
 if __name__ == '__main__':
-    for grid in [search_spaces_1]:
-        for sp in ParameterGrid(grid):
-            # for _ in [1]:
-            flags = parser.parse_args()
-            flags_setup = FlagsSetup(get_config_path(flags))
-            flags = flags_setup.setup(flags, additional_args=sp)
-            # flags = flags_setup.setup(flags)
-            mst = PolymnistExperiment(flags)
-            mst.set_optimizer()
-            trainer = PolymnistTrainer(mst)
-            trainer.run_epochs()
+    with norby():
+        for grid in [search_spaces]:
+            for sp in ParameterGrid(grid):
+                # for _ in [1]:
+                flags = parser.parse_args()
+                flags_setup = FlagsSetup(get_config_path(flags))
+                flags = flags_setup.setup(flags, additional_args=sp)
+                # flags = flags_setup.setup(flags)
+                mst = PolymnistExperiment(flags)
+                mst.set_optimizer()
+                trainer = PolymnistTrainer(mst)
+                trainer.run_epochs()
