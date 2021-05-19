e131631954890346c66d7acb301cbfb987cd114a
klugh <klugh@student.ethz.ch>
2021-05-19 15:36:19 +0200

    clean up testing scripts

---
+++ tests/test_static.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+import tempfile
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from mmvae_hub.base.utils.utils import json2dict, write_to_jsonfile
+from mmvae_hub.polymnist import PolymnistTrainer
+from tests.utils import set_me_up
+
+
+@pytest.mark.tox
+@pytest.mark.parametrize("method", ['moe', 'joint_elbo'])
+def test_static_results_1mod(method: str, update_static_results=False):
+    """
+    Test if the results are constant. If the assertion fails, it means that the model or the evaluation has
+    changed, perhaps involuntarily.
+    """
+    jsonfile = Path(__file__).parent / 'static_results.json'
+    static_results = json2dict(jsonfile)['static_results_1mod'][method]
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        mst = set_me_up(tmpdirname,
+                        method=method,
+                        attributes={'num_flows': 0, 'num_mods': 1, 'deterministic': True, 'device': 'cpu',
+                                    'steps_per_training_epoch': 1, 'factorized_representation': False})
+        trainer = PolymnistTrainer(mst)
+        test_results = trainer.run_epochs()
+
+        if update_static_results:
+            static_results['joint_div'] = test_results.joint_div
+            static_results['klds'] = test_results.klds['m0']
+            static_results['lhoods'] = test_results.lhoods['m0']['m0']
+            static_results['log_probs'] = test_results.log_probs['m0']
+            static_results['total_loss'] = test_results.total_loss
+            static_results['lr_eval'] = test_results.lr_eval['m0']['accuracy']
+            static_results['latents_class'] = {}
+            static_results['latents_class']['mu'] = test_results.latents['m0']['latents_class']['mu']
+
+            write_to_jsonfile(jsonfile, [(f'static_results_1mod.{method}', static_results)])
+
+        are_they_equal = {
+            'joint_div': test_results.joint_div + 1 == static_results['joint_div'],
+            'klds': test_results.klds['m0'] == static_results['klds'],
+            'lhoods': test_results.lhoods['m0']['m0'] == static_results['lhoods'],
+            'log_probs': test_results.log_probs['m0'] == static_results['log_probs'],
+            'total_loss': test_results.total_loss == static_results['total_loss'],
+            'lr_eval': test_results.lr_eval['m0']['accuracy'] == static_results['lr_eval'],
+            'latents_class_mu': test_results.latents['m0']['latents_class']['mu'] == static_results['latents_class'][
+                'mu']
+        }
+
+        assert all(v for _, v in are_they_equal.items()), f'Some results changed: {are_they_equal}'
+
+
+# @pytest.mark.tox
+@pytest.mark.parametrize("method", ['moe', 'joint_elbo'])
+def test_static_results_2mods(method: str):
+    """
+    Test if the results are constant. If the assertion fails, it means that the model or the evaluation has
+    changed, perhaps involuntarily.
+    """
+    static_results = json2dict(Path('static_results.json'))['static_results_2mod']
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        mst = set_me_up(tmpdirname,
+                        method=method,
+                        attributes={'num_flows': 0, 'num_mods': 2, 'deterministic': True, 'device': 'cpu',
+                                    'steps_per_training_epoch': 1, 'factorized_representation': False})
+        trainer = PolymnistTrainer(mst)
+        test_results = trainer.run_epochs()
+        assert np.round(test_results.joint_div, 1) == np.round(static_results[method]['joint_div'], 1)
+        assert np.round(test_results.klds['m0'], 1) == np.round(static_results[method]['klds'], 1)
+        assert np.round(test_results.lhoods['m0']['m0'], 1) == np.round(static_results[method]['lhoods'], 1)
+        assert np.round(test_results.log_probs['m0'], 0) == np.round(static_results[method]['log_probs'], 0)
+        assert np.round(test_results.total_loss, 0) == np.round(static_results[method]['total_loss'], 0)
+        assert np.round(test_results.lr_eval['m0']['accuracy'], 2) == np.round(static_results[method]['lr_eval'], 2)
+        assert np.round(test_results.latents['m0']['latents_class']['mu'], 2) == np.round(
+            static_results[method]['latents_class']['mu'], 2)
+
+
+if __name__ == '__main__':
+    test_static_results_1mod('moe', update_static_results=False)
+    test_static_results_1mod('joint_elbo', update_static_results=False)
--- tests/test_training.py
+++ tests/test_training.py
@@ -1,6 +1,5 @@
 import tempfile
 
-import numpy as np
 import pytest
 
 from mmvae_hub.base.evaluation.eval_metrics.coherence import test_generation
@@ -19,71 +18,18 @@ def test_run_epochs_polymnist(method: str):
     changed, perhaps involuntarily.
     """
     with tempfile.TemporaryDirectory() as tmpdirname:
-        mst = set_me_up(tmpdirname, method)
+        if method in ['planar_mixture', 'pfom']:
+            # todo implement calc likelihood for flow based methods
+            calc_nll = False
+        mst = set_me_up(tmpdirname, method, attributes={'calc_nll': calc_nll})
         trainer = PolymnistTrainer(mst)
         test_results = trainer.run_epochs()
-
-
-# @pytest.mark.tox
-@pytest.mark.parametrize("method", ['moe', 'joint_elbo'])
-def test_static_results_2mods(method: str):
-    """
-    Test if the results are constant. If the assertion fails, it means that the model or the evaluation has
-    changed, perhaps involuntarily.
-    """
-    static_results = {
-        'moe': {"joint_div": 0.24978025257587433,
-                "latents_class": {'mu': -0.0019471765263006091},
-                "klds": 0.2534570097923279,
-                "lhoods": -2613.809326171875,
-                "log_probs": 2616.397705078125,
-                "total_loss": 5248.791015625,
-                "lr_eval": 0.05
-                },
-        'planar_mixture': {"joint_div": -9.384511947631836,
-                           "latents_class": {'mu': 0.2744814455509186},
-                           "klds": 0.2744814455509186,
-                           "lhoods": -2613.845703125,
-                           "log_probs": 2616.397216796875,
-                           "total_loss": 2617.664306640625,
-                           "lr_eval": 0.05
-                           },
-        'joint_elbo': {"joint_div": 0.9404307007789612,
-                       "latents_class": {'mu': -0.000974901719018817},
-                       "klds": 0.25186216831207275,
-                       "lhoods": -2613.82275390625,
-                       "log_probs": 2616.86767578125,
-                       "total_loss": 5253.03369140625,
-                       "lr_eval": 0.05
-                       },
-
-    }
-
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        # mst = set_me_up(tmpdirname, method='moe',
-        #                 attributes={'num_flows': 0, 'num_mods': 1, 'deterministic': True, 'device': 'cpu'})
-        mst = set_me_up(tmpdirname,
-                        method=method,
-                        attributes={'num_flows': 0, 'num_mods': 2, 'deterministic': True, 'device': 'cpu',
-                                    'steps_per_training_epoch': 1, 'factorized_representation': False})
-        trainer = PolymnistTrainer(mst)
-        test_results = trainer.run_epochs()
-        assert np.round(test_results.joint_div, 1) == np.round(static_results[method]['joint_div'], 1)
-        assert np.round(test_results.klds['m0'], 1) == np.round(static_results[method]['klds'], 1)
-        assert np.round(test_results.lhoods['m0']['m0'], 1) == np.round(static_results[method]['lhoods'], 1)
-        assert np.round(test_results.log_probs['m0'], 0) == np.round(static_results[method]['log_probs'], 0)
-        assert np.round(test_results.total_loss, 0) == np.round(static_results[method]['total_loss'], 0)
-        assert np.round(test_results.lr_eval['m0']['accuracy'], 2) == np.round(static_results[method]['lr_eval'], 2)
-        assert np.round(test_results.latents['m0']['latents_class']['mu'], 2) == np.round(
-            static_results[method]['latents_class']['mu'], 2)
 
 
 @pytest.mark.tox
 def test_run_planar_mixture_no_flow():
     """
     Test if the main training loop runs.
-    Assert if the total_test loss is constant. If the assertion fails, it means that the model or the evaluation has
-    changed, perhaps involuntarily.
     """
     with tempfile.TemporaryDirectory() as tmpdirname:
         method = 'planar_mixture'
@@ -107,11 +53,11 @@ def test_test_generation():
 
 if __name__ == '__main__':
     # pass
-    # test_static_results_2mods('joint_elbo')
-    # test_static_results_2mods('moe')
+
     # test_run_epochs_polymnist(method='joint_elbo')
     # test_run_epochs_polymnist(method='moe')
     test_run_epochs_polymnist(method='planar_mixture')
+    test_run_epochs_polymnist(method='pfom')
     # test_run_planar_mixture_no_flow()
     # test_generate_plots()
     # test_test_generation()
