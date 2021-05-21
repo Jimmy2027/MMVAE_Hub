bbd417f8341878a3d755e29ecadae23bce17619e
klugh <klugh@student.ethz.ch>
2021-05-21 22:27:48 +0200

    add dataset extraction for leomed

--- mmvae_hub/polymnist/main_polymnist.py
+++ mmvae_hub/polymnist/main_polymnist.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
+import zipfile
+from pathlib import Path
+
 from norby.utils import maybe_norby
-from sklearn.model_selection import ParameterGrid
 
 from mmvae_hub.base.utils.flags_utils import get_config_path
 from mmvae_hub.polymnist import PolymnistTrainer
@@ -8,9 +11,22 @@ from mmvae_hub.polymnist.experiment import PolymnistExperiment
 from mmvae_hub.polymnist.flags import FlagsSetup, parser
 
 if __name__ == '__main__':
+
     flags = parser.parse_args()
     flags_setup = FlagsSetup(get_config_path(flags))
     flags = flags_setup.setup(flags)
+
+    if Path('/cluster').exists():
+        polymnist_zip_path = Path('/cluster/work/vogtlab/Projects/Polymnist/PolyMNIST.zip')
+        tmpdir = Path(os.getenv("TMPDIR"))
+        out_dir = tmpdir / 'polymnist'
+        out_dir.mkdir()
+
+        with zipfile.ZipFile(polymnist_zip_path) as z:
+            z.extractall(str(out_dir))
+
+        flags.dir_data = out_dir
+
     with maybe_norby(flags.norby, f'Starting Experiment {flags.experiment_uid}.',
                      f'Experiment {flags.experiment_uid} finished.'):
         flags = flags_setup.setup(flags)
