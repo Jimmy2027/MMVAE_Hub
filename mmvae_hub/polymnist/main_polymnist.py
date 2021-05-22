a3879bd30c3345f15d05c552f6ff59dfbe64f8c5
klugh <klugh@student.ethz.ch>
2021-05-22 10:52:27 +0200

    cleanup

--- mmvae_hub/polymnist/main_polymnist.py
+++ mmvae_hub/polymnist/main_polymnist.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from pathlib import Path
 
 from norby.utils import maybe_norby
@@ -10,18 +8,13 @@ from mmvae_hub.polymnist.experiment import PolymnistExperiment
 from mmvae_hub.polymnist.flags import FlagsSetup, parser
 
 if __name__ == '__main__':
-    if Path('/cluster').exists():
-        leomed = True
-    else:
-        leomed = False
 
     flags = parser.parse_args()
     flags_setup = FlagsSetup(get_config_path(flags))
-    flags = flags_setup.setup(flags, leomed=leomed)
+    flags = flags_setup.setup(flags)
 
     with maybe_norby(flags.norby, f'Starting Experiment {flags.experiment_uid}.',
                      f'Experiment {flags.experiment_uid} finished.'):
-        flags = flags_setup.setup(flags)
         mst = PolymnistExperiment(flags)
         mst.set_optimizer()
         trainer = PolymnistTrainer(mst)
