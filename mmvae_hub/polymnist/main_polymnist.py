0ba3062d7b25e51d6d0daf73abc6ee968af205e9
klugh <klugh@student.ethz.ch>
2021-05-22 11:07:38 +0200

    add leomed zip experiment_dir

--- mmvae_hub/polymnist/main_polymnist.py
+++ mmvae_hub/polymnist/main_polymnist.py
@@ -1,8 +1,10 @@
+import shutil
 from pathlib import Path
 
 from norby.utils import maybe_norby
 
 from mmvae_hub.base.utils.flags_utils import get_config_path
+from mmvae_hub.base.utils.utils import json2dict
 from mmvae_hub.polymnist import PolymnistTrainer
 from mmvae_hub.polymnist.experiment import PolymnistExperiment
 from mmvae_hub.polymnist.flags import FlagsSetup, parser
@@ -19,3 +21,10 @@ if __name__ == '__main__':
         mst.set_optimizer()
         trainer = PolymnistTrainer(mst)
         trainer.run_epochs()
+
+    # move zipped experiment_dir_run in TMPDIR to experiment_dir
+    if flags.leomed:
+        dir_experiment = json2dict(get_config_path())['dir_experiment']
+
+        # zip dir_experiment_run
+        shutil.make_archive(Path(dir_experiment) / flags.experiment_uid, 'zip', flags.dir_experiment_run)
