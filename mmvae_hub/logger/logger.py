d4f867de6b2c988f508ccdb50cf20ac3f44a3603
klugh <klugh@student.ethz.ch>
2021-05-24 22:13:57 +0200

    reduce nbr stored logs

--- mmvae_hub/logger/logger.py
+++ mmvae_hub/logger/logger.py
@@ -27,7 +27,7 @@ except Exception as e:
     LOG_FILE = (pathlib.Path(__file__).parent / "logs")
     LOG_FILE.mkdir(exist_ok=True, parents=True)
 
-for f in sorted(LOG_FILE.glob("*-*-*.log"))[:-10]:
+for f in sorted(LOG_FILE.glob("*-*-*.log"))[:-5]:
     os.remove(f)
 
 LOG_FILE = LOG_FILE / datetime.datetime.now(tz=datetime.timezone.utc).strftime("%Z-%Y%m%d-%H%M%S")
