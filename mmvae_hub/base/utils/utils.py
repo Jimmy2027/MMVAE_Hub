3cddaeeb4c623a65d7a9dbd20fea539843c64ac2
klugh <klugh@student.ethz.ch>
2021-05-25 11:30:03 +0200

    move mixture selection methon

--- mmvae_hub/base/utils/utils.py
+++ mmvae_hub/base/utils/utils.py
@@ -47,40 +47,6 @@ def get_items_from_dict(in_dict: Mapping[str, Tensor]) -> Mapping[str, float]:
     return {k1: v1.cpu().item() for k1, v1 in in_dict.items()}
 
 
-def mixture_component_selection(flags, mus, logvars, w_modalities=None) -> Distr:
-    """
-    For every sample, select one of the experts. Return the joint distribution as mixture of experts.
-
-    Every experts gets selected with probability proportional to the corresponding w_modality, this is equivalent to
-    taking an expert for a proportion of the batch that is equal to w_modality —which is what is done here.
-    This simulates sampling from the sum of experts, which would not be Gaussian.
-    """
-    num_components = mus.shape[0]
-    # num_samples is the batch_size
-    num_samples = mus.shape[1]
-    # if not defined, take pre-defined weights
-    if w_modalities is None:
-        w_modalities = torch.Tensor(flags.alpha_modalities).to(flags.device)
-
-    idx_start = []
-    idx_end = []
-    for k in range(num_components):
-        i_start = 0 if k == 0 else int(idx_end[k - 1])
-        if k == w_modalities.shape[0] - 1:
-            i_end = num_samples
-        else:
-            i_end = i_start + int(torch.floor(num_samples * w_modalities[k]))
-        idx_start.append(i_start)
-        idx_end.append(i_end)
-
-    idx_end[-1] = num_samples
-
-    mu_sel = torch.cat([mus[k, idx_start[k]:idx_end[k], :] for k in range(w_modalities.shape[0])])
-    logvar_sel = torch.cat([logvars[k, idx_start[k]:idx_end[k], :] for k in range(w_modalities.shape[0])])
-
-    return Distr(mu=mu_sel, logvar=logvar_sel)
-
-
 def save_and_log_flags(flags) -> str:
     filename_flags_rar = os.path.join(flags.dir_experiment_run, 'flags.rar')
     torch.save(flags, filename_flags_rar)
