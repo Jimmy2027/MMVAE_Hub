2822edf61c02cbd2e6e54d57a1edf4eec44bf4a1
klugh <klugh@student.ethz.ch>
2021-05-28 14:52:24 +0200

    update search params

--- mmvae_hub/polymnist/leomed_run.py
+++ mmvae_hub/polymnist/leomed_run.py
@@ -35,15 +35,16 @@ search_space_planar_mixture = {
 search_space_planar_pfom = {
     'n_gpus': [1],
     'method': ['pfom'],
-    'beta': [1, 2.5],
+    'beta': [0.5, 1, 2.5],
     "num_mods": [3],
     "num_flows": [5],
-    "end_epoch": [900],
-    "weighted_mixture": [True]
+    "end_epoch": [600],
+    "weighted_mixture": [False],
+    "amortized_flow": [False]
 }
 
 # for search_space in [search_space_joint_elbo, search_space_moe, search_space_planar_mixture]:
-for search_space in [search_space_planar_mixture]:
+for search_space in [search_space_planar_mixture, search_space_planar_pfom]:
 
     for params in ParameterGrid(search_space):
 
@@ -55,7 +56,7 @@ for search_space in [search_space_planar_mixture]:
             params['gpu_mem'] = 5000
 
         # 100 epochs need a bit less than 2 hours
-        num_hours = int((params['end_epoch'] // 100) * 2)
+        num_hours = int((params['end_epoch'] // 100) * 2) or 1
 
         command = f'bsub -n 8 -W {num_hours}:00 -R "rusage[mem=1000,ngpus_excl_p={params["n_gpus"]},scratch=2000]" ' \
                   f'-R "select[gpu_mtotal0>={params["gpu_mem"] * params["n_gpus"]}]" ' \
