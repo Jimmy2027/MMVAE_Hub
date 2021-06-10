6be113df869392b47acd6e7ccba07cab054d13dc
klugh <klugh@student.ethz.ch>
2021-06-10 15:43:41 +0200

    update leomed_run

--- mmvae_hub/polymnist/leomed_run.py
+++ mmvae_hub/polymnist/leomed_run.py
@@ -4,47 +4,62 @@ import time
 
 from sklearn.model_selection import ParameterGrid
 
-search_space_joint_elbo = {
-    'n_gpus': [1],
-    'method': ['joint_elbo'],
-    'beta': [2.5],
-    "num_mods": [3],
-    "end_epoch": [900],
-}
-
-search_space_moe = {
-    'n_gpus': [1],
-    'method': ['moe'],
-    'beta': [2.5],
-    "num_mods": [3],
-    "end_epoch": [900],
-}
-
-search_space_planar_mixture = {
-    'n_gpus': [1],
-    'method': ['planar_mixture'],
-    'beta': [0.5, 1, 2.5],
+search_spaces_poe = {
+    'method': ['poe'],
     'class_dim': [256],
-    "num_mods": [3],
+    "beta": [1],
     "num_flows": [5],
-    "end_epoch": [600],
+    "num_mods": [3],
+    "end_epoch": [100],
     "weighted_mixture": [False],
     "amortized_flow": [False]
 }
 
-search_space_planar_pfom = {
-    'n_gpus': [1],
-    'method': ['pfom'],
-    'beta': [0.5, 1, 2.5],
-    "num_mods": [3],
+search_spaces_moe = {
+    'method': ['moe'],
+    'class_dim': [256],
+    "beta": [1],
     "num_flows": [5],
-    "end_epoch": [600],
+    "num_mods": [3],
+    "end_epoch": [100],
     "weighted_mixture": [False],
     "amortized_flow": [False]
 }
+
+search_spaces_je = {
+    'method': ['joint_elbo'],
+    'class_dim': [256],
+    "beta": [1],
+    "num_flows": [5],
+    "num_mods": [3],
+    "end_epoch": [100],
+    "weighted_mixture": [False],
+    "amortized_flow": [False]
+}
+search_spaces_pm = {
+    'method': ['planar_mixture'],
+    'class_dim': [256],
+    "beta": [1],
+    "num_flows": [5],
+    "num_mods": [3],
+    "end_epoch": [100],
+    "weighted_mixture": [False],
+    "amortized_flow": [False]
+}
+search_spaces_pfom = {
+    'method': ['pfom'],
+    'class_dim': [256],
+    "beta": [1],
+    "num_flows": [5],
+    "num_mods": [3],
+    "end_epoch": [100],
+    "weighted_mixture": [False],
+    "amortized_flow": [False]
+}
+
 
 # for search_space in [search_space_joint_elbo, search_space_moe, search_space_planar_mixture]:
-for search_space in [search_space_planar_mixture, search_space_planar_pfom]:
+for search_space in [search_spaces_poe, search_spaces_moe, search_spaces_je, search_spaces_pm, search_spaces_pfom]:
 
     for params in ParameterGrid(search_space):
 
@@ -54,13 +69,23 @@ for search_space in [search_space_planar_mixture, search_space_planar_pfom]:
         )
         if 'gpu_mem' not in params:
             params['gpu_mem'] = 5000
+        if 'n_gpus' not in params:
+            params['n_gpus'] = 1
 
         # 100 epochs need a bit less than 2 hours
         num_hours = int((params['end_epoch'] // 100) * 2) or 1
 
-        command = f'bsub -n 8 -W {num_hours}:00 -R "rusage[mem=1000,ngpus_excl_p={params["n_gpus"]},scratch=2000]" ' \
+        # 100 epochs take about 5G of space
+        scratch_space = int((params['end_epoch'] // 100) * 5) // 8
+
+        if 'optuna' in params and params['optuna']:
+            python_file = 'hyperopt/HyperoptTrainer.py'
+        else:
+            python_file = 'polymnist/main_polymnist.py'
+
+        command = f'bsub -n 8 -W {num_hours}:00 -R "rusage[mem=1000,ngpus_excl_p={params["n_gpus"]},scratch={scratch_space}]" ' \
                   f'-R "select[gpu_mtotal0>={params["gpu_mem"] * params["n_gpus"]}]" ' \
-                  f'python polymnist/main_polymnist.py {flags}'
+                  f'python {python_file} {flags}'
 
         # add boolean flags
         if 'factorized_representation' in params and params['factorized_representation']:
