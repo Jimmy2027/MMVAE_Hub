4af9a83a60640d7fe58f133dc5fee6cf3ae0eb7e
klugh <klugh@student.ethz.ch>
2021-06-10 15:57:36 +0200

    update from 0.0.9-dev

--- mmvae_hub/experiment_vis/utils.py
+++ mmvae_hub/experiment_vis/utils.py
@@ -15,6 +15,7 @@ from mmvae_hub.polymnist.experiment import PolymnistExperiment
 from mmvae_hub.utils.MongoDB import MongoDatabase
 from mmvae_hub.utils.flags_utils import BaseFlagsSetup, get_config_path
 from mmvae_hub.utils.plotting import generate_plots
+
 from mmvae_hub.utils.utils import dict2json
 
 
@@ -86,27 +87,70 @@ def write_experiment_vis_config(experiment_dir: Path) -> Path:
 
 
 def get_epochs(logs_dict: dict, metric: str):
+    """Get all the epochs the metric was evaluated."""
     return sorted([int(epoch) for epoch in logs_dict['epoch_results'] if
-                   logs_dict['epoch_results'][epoch]['test_results'][metric]])
+                   logs_dict['epoch_results'][epoch]['test_results'][metric] is not None])
 
 
 def plot_lr_accuracy(logs_dict: dict) -> None:
+    """
+    Plot the latent representation evaluation accuracy.
+    For method planar mixture, show both evaluations, before and after applying flows.
+    """
+    method = logs_dict['flags']['method']
     lr_accuracy_values = {}
-    epochs = get_epochs(logs_dict, 'lr_eval')
+
+    # for backwards compatibility:
+    if 'lr_eval_q0' not in logs_dict['epoch_results']['0']['test_results']:
+        for epoch in logs_dict['epoch_results']:
+            test_results = logs_dict['epoch_results'][epoch]['test_results']
+            if test_results['lr_eval'] is not None:
+                if method in ['planar_mixture', 'pfom']:
+                    test_results['lr_eval_zk'] = test_results['lr_eval']
+                    test_results['lr_eval_q0'] = {k: {'accuracy': None} for k in test_results['lr_eval']}
+                else:
+                    test_results['lr_eval_q0'] = test_results['lr_eval']
+                logs_dict['epoch_results'][epoch]['test_results'] = test_results
+            else:
+                logs_dict['epoch_results'][epoch]['test_results']['lr_eval_q0'] = None
+                logs_dict['epoch_results'][epoch]['test_results']['lr_eval_zk'] = None
+
+    epochs = get_epochs(logs_dict, metric='lr_eval_q0' if method != 'planar_mixture' else 'lr_eval_zk')
+
+    if method == 'pfom':
+        lr_keys = ['q0', 'zk']
+    elif method == 'planar_mixture':
+        lr_keys = ['zk']
+    else:
+        lr_keys = ['q0']
 
     for epoch, epoch_values in logs_dict['epoch_results'].items():
-        if epoch_values['test_results']['lr_eval']:
-            for k, v in epoch_values['test_results']['lr_eval'].items():
-                if k not in lr_accuracy_values:
-                    lr_accuracy_values[k] = [v['accuracy']]
-                else:
-                    lr_accuracy_values[k].append(v['accuracy'])
+        for lr_key in lr_keys:
+            if lr_key not in lr_accuracy_values:
+                lr_accuracy_values[lr_key] = {}
+            if epoch_values['test_results'][f'lr_eval_{lr_key}']:
+                for k, v in epoch_values['test_results'][f'lr_eval_{lr_key}'].items():
+                    # insert accuracy value to dict
+                    if k not in lr_accuracy_values[lr_key]:
+                        lr_accuracy_values[lr_key][k] = [v['accuracy']]
+                    else:
+                        lr_accuracy_values[lr_key][k].append(v['accuracy'])
 
-    plt.figure(figsize=(15, 10))
-    plt.title('Latent Representation Accuracy')
-    for subset, values in lr_accuracy_values.items():
-        plt.plot(epochs, values)
-    plt.legend([s for s in lr_accuracy_values])
+    if method == 'pfom':
+        fig, axs = plt.subplots(2, 1, figsize=(15, 10))
+        fig.suptitle('Latent Representation Accuracy')
+        for ax_idx, (lr_key, lr_values) in enumerate(lr_accuracy_values.items()):
+            for subset, values in lr_values.items():
+                axs[ax_idx].plot(epochs, values)
+            axs[ax_idx].legend([s for s in lr_values])
+            axs[ax_idx].set_title(lr_key)
+    else:
+        lr_key = lr_keys[0]
+        plt.figure(figsize=(15, 10))
+        plt.title('Latent Representation Accuracy')
+        for subset, values in lr_accuracy_values[lr_key].items():
+            plt.plot(epochs, values)
+        plt.legend([s for s in lr_accuracy_values[lr_key]])
     plt.show()
 
 
@@ -126,7 +170,7 @@ def plot_likelihoods(logs_dict: dict) -> None:
                         lhoods[subset][mod] = [result]
 
     for subset, v in lhoods.items():
-        plt.figure(figsize=(10, 5))
+        plt.figure(figsize=(15, 5))
         plt.title(f'Likelihoods for subset {subset}.')
         for mod, values in v.items():
             plt.plot(epochs, values)
@@ -216,11 +260,14 @@ def plot_coherence_accuracy(logs_dict: dict) -> None:
         plt.show()
 
 
-def show_generated_figs(experiment_dir: Path = None, flags=None):
+def show_generated_figs(experiment_dir: Path = None, flags=None, _id: str = None):
     if not flags:
         flags_setup = BaseFlagsSetup(get_config_path())
-        flags = flags_setup.load_old_flags(Path('flags.rar'), add_args={'save_figure': False})
-
+        flags_path = Path('flags.rar')
+        if flags_path.exists():
+            flags = flags_setup.load_old_flags(flags_path, add_args={'save_figure': False})
+        else:
+            flags = flags_setup.load_old_flags(_id=_id, add_args={'save_figure': False})
     exp = get_experiment(flags)
 
     if experiment_dir and (experiment_dir / 'checkpoints').exists():
@@ -244,58 +291,102 @@ def show_generated_figs(experiment_dir: Path = None, flags=None):
             plt.show()
 
 
+def bw_compat_epoch_results(epoch_results: dict, method: str) -> dict:
+    """
+    Adapt epoch results for backwards compatibility.
+    """
+    # some experiments have 'lr_eval_qk' instead of 'lr_eval_zk'
+    if 'lr_eval_qk' in epoch_results:
+        epoch_results['lr_eval_zk'] = epoch_results['lr_eval_qk']
+
+    if 'lr_eval_q0' not in epoch_results:
+        if method in ['planar_mixture', 'pfom']:
+            epoch_results['lr_eval_zk'] = epoch_results['lr_eval']
+            epoch_results['lr_eval_q0'] = {k: {'accuracy': None} for k in epoch_results['lr_eval']}
+        else:
+            epoch_results['lr_eval_q0'] = epoch_results['lr_eval']
+
+    return epoch_results
+
+
 def make_experiments_dataframe(experiments):
+    """
+    Create a dataframe with all experiment results.
+    A score is created by summing up all metrics from the lr_eval, the gen_val and the prd_eval. The score is
+    normalized by the number of modalities.
+    """
     df = pd.DataFrame()
     for exp in experiments.find({}):
         if exp['epoch_results'] is not None and exp['epoch_results']:
+            method = exp['flags']['method']
             max_epoch = max(int(epoch) for epoch in exp['epoch_results'])
 
             # get the last epoch where evaluation was run.
             if (max_epoch - max_epoch % int(exp['flags']['eval_freq']) > 1
-                    and exp['epoch_results'][str(max_epoch)]['test_results']['lr_eval'] is None):
+                    and exp['epoch_results'][str(max_epoch)]['test_results']['gen_eval'] is None):
                 last_epoch = str(max_epoch - max_epoch % int(exp['flags']['eval_freq']) - 1)
             else:
                 last_epoch = str(max_epoch)
             last_epoch_results = exp['epoch_results'][last_epoch]['test_results']
 
-            if exp['flags']['method'] in ['planar_mixture', 'pfom']:
+            if method in ['planar_mixture', 'pfom']:
                 exp['flags']['method'] = f"{exp['flags']['method']}_{exp['flags']['num_flows']}"
 
-            if last_epoch_results['lr_eval'] and last_epoch_results['gen_eval']:
+            # for backwards compatibility:
+            last_epoch_results = bw_compat_epoch_results(last_epoch_results, method)
 
+            # if lr_eval and gen_eval, add results to df.
+            if (last_epoch_results['lr_eval_q0'] or last_epoch_results['lr_eval_zk']) \
+                    and last_epoch_results['gen_eval']:
                 results_dict = {**exp['flags'], 'end_epoch': last_epoch, '_id': exp['_id']}
+                try:
+                    if 'expvis_url' in exp:
+                        results_dict['expvis_url'] = exp['expvis_url']
 
-                if 'expvis_url' in exp:
-                    results_dict['expvis_url'] = exp['expvis_url']
+                    scores = []
+                    scores_lr_q0 = []
+                    scores_lr_zk = []
 
-                scores = []
-                scores_lr = []
-                # get lr_eval results
-                for key, val in last_epoch_results['lr_eval'].items():
-                    results_dict[f'lr_eval_{key}'] = val['accuracy']
-                    scores.append(val['accuracy'])
-                    scores_lr.append(val['accuracy'])
+                    # get lr_eval results
+                    for key, val in last_epoch_results['lr_eval_q0'].items():
+                        results_dict[f'lr_eval_q0_{key}'] = val['accuracy']
+                        scores_lr_q0.append(val['accuracy'])
+                        if method not in ['planar_mixture', 'pfom']:
+                            scores.append(val['accuracy'])
 
-                scores_gen = []
-                # get gen_eval results
-                for key, val in last_epoch_results['gen_eval'].items():
-                    key = key.replace('digit_', '')
-                    results_dict[f'gen_eval_{key}'] = val
-                    scores.append(val)
-                    scores_gen.append(val)
+                    if method in ['planar_mixture', 'pfom', 'pope']:
+                        for key, val in last_epoch_results['lr_eval_zk'].items():
+                            results_dict[f'lr_eval_zk_{key}'] = val['accuracy']
+                            scores.append(val['accuracy'])
+                            scores_lr_zk.append(val['accuracy'])
 
-                scores_prd = []
+                    scores_gen = []
+                    # get gen_eval results
+                    for key, val in last_epoch_results['gen_eval'].items():
+                        key = key.replace('digit_', '')
+                        results_dict[f'gen_eval_{key}'] = val
+                        scores.append(val)
+                        scores_gen.append(val)
 
-                if 'prd_scores' in last_epoch_results and last_epoch_results['prd_scores']:
-                    for key, val in last_epoch_results['prd_scores'].items():
-                        results_dict[f'prd_score_{key}'] = val
-                        scores_prd.append(val)
+                    scores_prd = []
+                    # get prd scores
+                    if 'prd_scores' in last_epoch_results and last_epoch_results['prd_scores']:
+                        for key, val in last_epoch_results['prd_scores'].items():
+                            results_dict[f'prd_score_{key}'] = val
+                            scores_prd.append(val)
 
-                results_dict['score'] = sum(scores)
-                results_dict['score_lr'] = sum(scores_lr)
-                results_dict['score_gen'] = sum(scores_gen)
-                results_dict['score_prd'] = sum(scores_prd)
-                df = df.append(results_dict, ignore_index=True)
+                    results_dict['score'] = sum(scores) / results_dict['num_mods']
+                    results_dict['score_lr_q0'] = sum(scores_lr_q0) if scores_lr_q0[0] is not None else None
+                    results_dict['score_lr_zk'] = sum(scores_lr_zk) if scores_lr_zk else None
+                    results_dict['score_lr'] = results_dict['score_lr_zk'] if method in ['planar_mixture', 'pfom'] \
+                        else results_dict['score_lr_q0']
+                    results_dict['score_gen'] = sum(scores_gen)
+                    results_dict['score_prd'] = sum(scores_prd)
+                    df = df.append(results_dict, ignore_index=True)
+
+                except Exception as e:
+                    print(e)
+
         else:
             print(f'skipping experiment {exp["_id"]}')
     return df
@@ -314,10 +405,11 @@ def get_experiment(flags):
 
 
 if __name__ == '__main__':
-    # experiment_uid = 'polymnist_planar_mixture_2021_05_25_18_55_51_488540'
+    # experiment_uid = 'polymnist_pope_2021_06_08_20_52_59_710466'
+    # show_generated_figs(_id=experiment_uid)
     # experiments_database = MongoDatabase(training=False, _id=experiment_uid)
     # experiment_dict = experiments_database.get_experiment_dict()
     # plot_lr_accuracy(experiment_dict)
     # df = make_experiments_dataframe(experiments_database.connect())
-    for id in ['polymnist_planar_mixture_2021_05_25_11_37_30_843750']:
+    for id in ['polymnist_pfom_2021_06_10_10_59_17_258140']:
         upload_notebook_to_db(id)
