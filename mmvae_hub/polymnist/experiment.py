5576fee890c30c61985607051d7f8db2d57bfa40
klugh <klugh@student.ethz.ch>
2021-06-11 09:35:58 +0200

    fix get_params for optimizer

--- mmvae_hub/polymnist/experiment.py
+++ mmvae_hub/polymnist/experiment.py
@@ -72,7 +72,8 @@ class PolymnistExperiment(BaseExperiment):
             params.extend(list(self.mm_vae.parameters()))
         elif hasattr(self.mm_vae, 'flow'):
             for p in ['u', 'w', 'b']:
-                params.append(getattr(self.mm_vae.flow, p))
+                if hasattr(self.mm_vae.flow, p):
+                    params.append(getattr(self.mm_vae.flow, p))
 
         optimizer = optim.Adam(params, lr=self.flags.initial_learning_rate, betas=(self.flags.beta_1,
                                                                                    self.flags.beta_2))
