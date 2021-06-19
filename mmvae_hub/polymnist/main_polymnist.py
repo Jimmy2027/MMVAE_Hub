1d1aa4d7cb3e32ac2e976b1e0b13d459ef6eeded
klugh <klugh@student.ethz.ch>
2021-06-19 17:41:42 +0200

    fix dataset param

--- mmvae_hub/polymnist/main_polymnist.py
+++ mmvae_hub/polymnist/main_polymnist.py
@@ -7,12 +7,13 @@ from mmvae_hub.polymnist.experiment import PolymnistExperiment
 from mmvae_hub.polymnist.flags import FlagsSetup, parser
 from mmvae_hub.utils.setup.flags_utils import get_config_path
 
+DATASET = 'polymnist'
+
 if __name__ == '__main__':
 
     flags = parser.parse_args()
-    flags.dataset = 'polymnist'
-    flags_setup = FlagsSetup(get_config_path(flags=flags))
-    flags = flags_setup.setup(flags)
+    flags_setup = FlagsSetup(get_config_path(dataset=DATASET, flags=flags))
+    flags = flags_setup.setup(flags, additional_args={'dataset': DATASET})
 
     with maybe_norby(flags.norby, f'Starting Experiment {flags.experiment_uid}.',
                      f'Experiment {flags.experiment_uid} finished.'):
