7ac9e556caf913f32894877bf51ce3f6afc29c80
klugh <klugh@student.ethz.ch>
2021-06-20 15:32:31 +0200

    fix syntax

--- mmvae_hub/hyperopt/leomed_launcher.sh
+++ mmvae_hub/hyperopt/leomed_launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bsub -n 8 -W 10:00 -R rusage[mem=1500,ngpus_excl_p=1,scratch=1000] -R select[gpu_mtotal0>=5000] python hyperopt/HyperoptTrainer.py
+bsub -n 8 -W 10:00 -R "rusage[mem=1500,ngpus_excl_p=1,scratch=1000]" -R "select[gpu_mtotal0>=5000]" python hyperopt/HyperoptTrainer.py
