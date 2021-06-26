89ad145d7f3aacfdf3846dd0baf472b1a17c0cad
klugh <klugh@student.ethz.ch>
2021-06-26 11:19:08 +0200

    add coupling_dim parameter

--- mmvae_hub/base/BaseFlags.py
+++ mmvae_hub/base/BaseFlags.py
@@ -117,3 +117,4 @@ parser.add_argument('--kl_annealing', type=int, default=0,
 parser.add_argument('--num_flows', type=int, default=4, help="Number of flow layers, ignored in absence of flows.")
 parser.add_argument('--amortized_flow', type=str2bool, default=False,
                     help="If True, use amortized flows, as described in Berg et al. 2019")
+parser.add_argument('--coupling_dim', type=int, default=512, help="Dimensions of the coupling layers in flow.")
