296dbf4c93978bd849147eb9d3b5fa49fc1e1027
klugh <klugh@student.ethz.ch>
2021-07-02 11:47:59 +0200

    add 0 flow option

--- mmvae_hub/networks/flows/AffineFlows.py
+++ mmvae_hub/networks/flows/AffineFlows.py
@@ -2,6 +2,7 @@
 
 import FrEIA.framework as Ff
 import FrEIA.modules as Fm
+import torch
 from torch import nn
 
 from mmvae_hub.utils.Dataclasses import PlanarFlowParams
@@ -12,17 +13,19 @@ class AffineFlow(nn.Module):
 
     def __init__(self, class_dim, num_flows, coupling_dim):
         super().__init__()
-
+        self.num_flows = num_flows
         self.coupling_dim = coupling_dim
-        # a simple chain of operations is collected by ReversibleSequential
-        # see here for more details: https://vll-hd.github.io/FrEIA/_build/html/FrEIA.modules.html#coupling-blocks
-        self.flow = Ff.SequenceINN(class_dim)
-        for _ in range(num_flows):
-            self.flow.append(Fm.AllInOneBlock, subnet_constructor=self.subnet_fc, permute_soft=True)
+        if num_flows > 0:
+            # a simple chain of operations is collected by ReversibleSequential
+            # see here for more details: https://vll-hd.github.io/FrEIA/_build/html/FrEIA.modules.html#coupling-blocks
+            self.flow = Ff.SequenceINN(class_dim)
+            for _ in range(num_flows):
+                self.flow.append(Fm.AllInOneBlock, subnet_constructor=self.subnet_fc, permute_soft=True)
 
     def forward(self, z0, flow_params=None):
+        if self.num_flows == 0:
+            return z0, torch.zeros_like(z0)
         zk, log_det_jacobian = self.flow(z0)
-
         return zk, log_det_jacobian
 
     def rev(self, zk):
