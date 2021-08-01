9fc90c99dc0a1de5b0bf6be458e9150828a69bd5
klugh <klugh@student.ethz.ch>
2021-08-01 17:45:26 +0200

    update

--- mmvae_hub/experiment_vis/utils.py
+++ mmvae_hub/experiment_vis/utils.py
@@ -5,6 +5,7 @@ import operator
 import shutil
 import tempfile
 from pathlib import Path
+from typing import Mapping
 
 import nbformat
 import numpy as np
@@ -17,9 +18,11 @@ from pandas import DataFrame
 
 from mmvae_hub import log
 from mmvae_hub.mimic.experiment import MimicExperiment
+from mmvae_hub.modalities import BaseModality
 from mmvae_hub.networks.BaseMMVae import BaseMMVAE
 from mmvae_hub.networks.FlowVaes import FlowVAE, FoMoP
 from mmvae_hub.polymnist.experiment import PolymnistExperiment
+from mmvae_hub.utils.Dataclasses import ReparamLatent
 from mmvae_hub.utils.MongoDB import MongoDatabase
 from mmvae_hub.utils.plotting.plotting import generate_plots
 from mmvae_hub.utils.setup.flags_utils import BaseFlagsSetup, get_config_path
@@ -100,6 +103,7 @@ def write_experiment_vis_config(experiment_dir: Path) -> Path:
 def get_epochs(logs_dict: dict, metric: str):
     """Get all the epochs the metric was evaluated."""
     return sorted([int(epoch) for epoch in logs_dict['epoch_results'] if
+                   logs_dict['epoch_results'][epoch]['test_results'] and
                    logs_dict['epoch_results'][epoch]['test_results'][metric] is not None])
 
 
@@ -112,8 +116,13 @@ def plot_lr_accuracy(logs_dict: dict) -> None:
     lr_accuracy_values = {}
 
     # for backwards compatibility:
-    if 'lr_eval_q0' not in logs_dict['epoch_results']['0']['test_results']:
+    if (
+            logs_dict['epoch_results']['0']['test_results'] is not None
+            and 'lr_eval_q0' not in logs_dict['epoch_results']['0']['test_results']
+    ):
         for epoch in logs_dict['epoch_results']:
+            if logs_dict['epoch_results'][epoch]['test_results'] is None:
+                logs_dict['epoch_results'][epoch]['test_results'] = {'lr_eval': None}
             test_results = logs_dict['epoch_results'][epoch]['test_results']
             if test_results['lr_eval'] is not None:
                 if method in ['planar_mixture', 'pfom']:
@@ -139,7 +148,7 @@ def plot_lr_accuracy(logs_dict: dict) -> None:
         for lr_key in lr_keys:
             if lr_key not in lr_accuracy_values:
                 lr_accuracy_values[lr_key] = {}
-            if epoch_values['test_results'][f'lr_eval_{lr_key}']:
+            if epoch_values['test_results'] and epoch_values['test_results'][f'lr_eval_{lr_key}']:
                 for k, v in epoch_values['test_results'][f'lr_eval_{lr_key}'].items():
                     # insert accuracy value to dict
                     if k not in lr_accuracy_values[lr_key]:
@@ -170,7 +179,7 @@ def plot_likelihoods(logs_dict: dict) -> None:
     epochs = get_epochs(logs_dict, 'lhoods')
 
     for epoch, epoch_values in logs_dict['epoch_results'].items():
-        if epoch_values['test_results']['lhoods']:
+        if epoch_values['test_results'] and epoch_values['test_results']['lhoods']:
             for subset, v_subset in epoch_values['test_results']['lhoods'].items():
                 if subset not in lhoods:
                     lhoods[subset] = {}
@@ -197,15 +206,16 @@ def plot_basic_batch_logs(phase: str, logs_dict: dict):
 
     for epoch, epoch_values in logs_dict['epoch_results'].items():
         v = epoch_values[f'{phase}_results']
-        results_dict['total_loss'].append(v['total_loss'])
-        results_dict['joint_divergence'].append(v['joint_divergence'])
+        if v:
+            results_dict['total_loss'].append(v['total_loss'])
+            results_dict['joint_divergence'].append(v['joint_divergence'])
 
-        for log_k in ['klds', 'log_probs']:
-            for s_key, s_value in v[log_k].items():
-                if s_key not in results_dict[log_k]:
-                    results_dict[log_k][s_key] = [s_value]
-                else:
-                    results_dict[log_k][s_key].append(s_value)
+            for log_k in ['klds', 'log_probs']:
+                for s_key, s_value in v[log_k].items():
+                    if s_key not in results_dict[log_k]:
+                        results_dict[log_k][s_key] = [s_value]
+                    else:
+                        results_dict[log_k][s_key].append(s_value)
 
     for k in ['total_loss', 'joint_divergence']:
         plt.figure(figsize=(10, 5))
@@ -228,7 +238,7 @@ def show_latents(logs_dict: dict) -> None:
     enc_mods_logvars = {}
 
     for epoch, epoch_values in logs_dict['epoch_results'].items():
-        if epoch_values['test_results']['latents']:
+        if epoch_values['test_results'] and epoch_values['test_results']['latents']:
             for mod_str, v in epoch_values['test_results']['latents'].items():
                 if mod_str not in enc_mods_mus:
                     enc_mods_mus[mod_str] = [v['latents_class']['mu']]
@@ -251,7 +261,7 @@ def plot_coherence_accuracy(logs_dict: dict) -> None:
     epochs = get_epochs(logs_dict, 'gen_eval')
 
     for epoch, epoch_values in logs_dict['epoch_results'].items():
-        if epoch_values['test_results']['gen_eval']:
+        if epoch_values['test_results'] and epoch_values['test_results']['gen_eval']:
             for k, v in epoch_values['test_results']['gen_eval'].items():
                 k = k.removeprefix('digit_')
                 num_input_mods = len(k.split('__')[0].split('_'))
@@ -310,7 +320,69 @@ def plot_betas(logs_dict: dict) -> None:
     plt.show()
 
 
-def show_generated_figs(experiment_dir: Path = None, flags=None, _id: str = None):
+def save_cond_gen(_id: str, save_path: Path):
+    """
+    Save examples of the cond gen to save_path.
+    """
+    exp = load_experiment(_id=_id)
+    exp.set_eval_mode()
+    model = exp.mm_vae
+    subsets = exp.subsets
+    mods: Mapping[str, BaseModality] = exp.modalities
+
+    test_samples = exp.test_samples
+
+    # get style from random sampling
+    random_styles = model.get_random_styles(1)
+
+    input_samples = {}
+
+    # save input images
+    for _, mod in mods.items():
+        input_sample = mod.plot_data(test_samples[0][mod.name])
+        input_samples[mod.name] = input_sample
+        plt.imshow(input_sample.cpu().moveaxis(0, -1))
+        plt.axis('off')
+        savefig_path = save_path / 'input_samples' / f'{mod.name}.png'
+        savefig_path.parent.mkdir(exist_ok=True, parents=True)
+        plt.title(f'{mod.name}', fontsize=30)
+        plt.savefig(savefig_path, bbox_inches='tight', pad_inches=0)
+        # plt.show()
+
+    for s_key in subsets:
+        subset = subsets[s_key]
+        i_batch = {
+            mod.name: input_samples[mod.name].unsqueeze(0)
+            for mod in subset
+        }
+        # infer latents from batch
+        enc_mods, joint_latent = model.inference(i_batch)
+
+        # c_rep is embedding of subset
+        c_rep = joint_latent.get_subset_embedding(s_key)
+
+        style = {}
+        for m_key_out in mods:
+            mod_out = mods[m_key_out]
+            if exp.flags.factorized_representation:
+                style[mod_out.name] = random_styles[mod_out.name][i].unsqueeze(0)
+            else:
+                style[mod_out.name] = None
+        cond_mod_in = ReparamLatent(content=c_rep, style=style)
+        cond_gen_samples = model.generate_from_latents(cond_mod_in)
+
+        for out_key, out_plot in cond_gen_samples.items():
+            plt.imshow(out_plot.squeeze().detach().cpu().moveaxis(0, -1))
+            plt.axis('off')
+            savefig_path = save_path / f'{s_key}' / f'{out_key}.png'
+            savefig_path.parent.mkdir(exist_ok=True, parents=True)
+            plt.title(f'{s_key} -> {out_key}', fontsize=30)
+            plt.savefig(savefig_path, bbox_inches='tight', pad_inches=0)
+            # plt.show()
+
+
+def load_experiment(experiment_dir: Path = None, flags=None, _id: str = None):
+    """Load experiment with flags and trained model from old experiment."""
     if not flags:
         dataset = _id.split('_')[0].lower()
         flags_setup = BaseFlagsSetup(get_config_path(dataset=dataset))
@@ -332,6 +404,12 @@ def show_generated_figs(experiment_dir: Path = None, flags=None, _id: str = None
         # load networks from database
         exp.mm_vae = exp.experiments_database.load_networks_from_db(exp.mm_vae)
 
+    return exp
+
+
+def show_generated_figs(experiment_dir: Path = None, flags=None, _id: str = None):
+    exp = load_experiment(experiment_dir, flags, _id)
+
     exp.set_eval_mode()
     plots = generate_plots(exp, epoch=0)
 
@@ -339,11 +417,12 @@ def show_generated_figs(experiment_dir: Path = None, flags=None, _id: str = None
         for name, fig in ps.items():
             plt.figure(figsize=(10, 10))
             plt.imshow(fig)
-            plt.title(p_key + '_' + name)
+            title = p_key + '_' + name
+            plt.title(title)
             plt.show()
 
 
-def bw_compat_epoch_results(epoch_results: dict, method: str, flags:dict):
+def bw_compat_epoch_results(epoch_results: dict, method: str, flags: dict):
     """
     Adapt epoch results for backwards compatibility.
     """
@@ -483,14 +562,17 @@ def display_base_params(df, methods: list, show_cols: list, num_flows: int = 5):
 
 if __name__ == '__main__':
     # experiment_uid = 'Mimic_joint_elbo_2021_07_06_09_44_52_871882'
-    experiment_uid = 'polymnist_mopgfm_2021_07_18_16_00_02_949672'
-    show_generated_figs(_id=experiment_uid)
+    # experiment_uid = 'polymnist_mopgfm_2021_07_31_12_05_21_355664'
+    # cond_gen(_id=experiment_uid, save_path='')
+    # show_generated_figs(_id=experiment_uid)
     # experiments_database = MongoDatabase(training=False, _id=experiment_uid)
     # experiment_dict = experiments_database.get_experiment_dict()
+    # plot_basic_batch_logs('train', experiment_dict)
+    # plot_basic_batch_logs('test', experiment_dict)
     # plot_betas(experiment_dict)
     # plot_lr_accuracy(experiment_dict)
     # df = make_experiments_dataframe(experiments_database.connect())
     # plot_prd_scores(pd.DataFrame(df.loc[df['_id'] == 'polymnist_pgfm_2021_07_09_21_52_29_311887']))
     # compare_methods(df, methods=['gfm', 'joint_elbo'], df_selectors={'end_epoch': 99})
-    # for id in ['polymnist_pgfm_2021_07_09_21_52_29_311887']:
-    #     upload_notebook_to_db(id)
+    for id in ['polymnist_mopgfm_2021_07_31_17_42_58_416337']:
+        upload_notebook_to_db(id)
