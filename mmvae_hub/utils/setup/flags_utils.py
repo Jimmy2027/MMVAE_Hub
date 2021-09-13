c2ad448471cc916671d0ad8306fb46b3b49d099a
klugh <klugh@student.ethz.ch>
2021-09-13 15:10:03 +0200

    add default nbr_coupling_block_layers value for backwards compat

--- mmvae_hub/utils/setup/flags_utils.py
+++ mmvae_hub/utils/setup/flags_utils.py
@@ -158,7 +158,7 @@ class BaseFlagsSetup:
         If flags_path is None, flags will be loaded from the db using the _id.
         """
         defaults = [('weighted_mixture', False), ('amortized_flow', False), ('coupling_dim', 512), ('beta_warmup', 0),
-                    ('vocab_size', 2900)]
+                    ('vocab_size', 2900), ('nbr_coupling_block_layers', 0)]
         add_args = add_args | {'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu')}
 
         if is_dict or flags_path is None:
